fc6e92264b32120b59ad48983646911b8825ccd8
Hanzala Habib <imhanzala444@gmail.com>
2025-07-31 10:46:11 +0500

    final version

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 class ApiService {
   static Future<List<dynamic>> getPosts() async {
     final response = await http.get(
-      Uri.parse('https://webhook.site/5a5fe8d4-0f05-414f-9407-808417c4739c'),
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
       headers: {
         "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/91.0.4472.124 Safari/537.36"
