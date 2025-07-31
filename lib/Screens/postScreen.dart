fe1759e5f7db03140bc677f732b9bf48e15c6972
Hanzala Habib <imhanzala444@gmail.com>
2025-07-31 10:18:43 +0500

    you can change the array of names to dynamic

--- lib/Screens/postScreen.dart
+++ lib/Screens/postScreen.dart
@@ -6,6 +6,18 @@ import '../controllers/post_list_controller.dart';
 
 class PostListView extends StatelessWidget {
   final controller = Get.put(PostListController());
+  List<String> names = [
+    'Alice', 'Bob', 'Charlie', 'David', 'Emma', 'Frank', 'Grace', 'Henry', 'Isla', 'Jack',
+    'Karen', 'Liam', 'Mia', 'Noah', 'Olivia', 'Paul', 'Quinn', 'Rachel', 'Steve', 'Tina',
+    'Uma', 'Victor', 'Wendy', 'Xavier', 'Yara', 'Zane', 'Aiden', 'Bella', 'Caleb', 'Daisy',
+    'Ethan', 'Fiona', 'George', 'Hannah', 'Ian', 'Jade', 'Kyle', 'Laura', 'Mason', 'Nina',
+    'Oscar', 'Penny', 'Quincy', 'Riley', 'Sam', 'Tara', 'Umar', 'Vera', 'Will', 'Xena',
+    'Yusuf', 'Zara', 'Amber', 'Brandon', 'Carmen', 'Derek', 'Elsa', 'Felix', 'Gina', 'Harvey',
+    'Ivy', 'Jake', 'Kylie', 'Leo', 'Megan', 'Nathan', 'Opal', 'Preston', 'Queen', 'Ron',
+    'Sophie', 'Tom', 'Ursula', 'Vincent', 'Whitney', 'Ximena', 'Yasmine', 'Zack', 'Annie',
+    'Bobby', 'Cleo', 'Dennis', 'Evelyn', 'Freddy', 'Gloria', 'Howard', 'Ingrid', 'Jonas',
+    'Kristen', 'Louis', 'Molly', 'Neil', 'Octavia', 'Peter', 'Rosa', 'Shane', 'Tracy', 'Wade'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +44,7 @@ class PostListView extends StatelessWidget {
           itemCount: controller.posts.length,
           itemBuilder: (context, index) {
             final PostModel post = controller.posts[index];
-            return ThreadCardTile(avatarUrl: "assets/man.png", post: post,  name: "${post.id}.Hanzala" , snippet:"${post.body}", dayAgo: "3d");
+            return ThreadCardTile(avatarUrl: "assets/man.png", post: post,  name: names[index] , snippet:"${post.body}", dayAgo: "3d");
           },
         );
       }),
